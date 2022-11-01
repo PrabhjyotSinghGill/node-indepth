65c9ac27e26592f65499222d15933d726d0a433a
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2022-11-01 14:55:01 -0400

    2.a) Node Module System: Global Object

---
+++ Node Module System/1)GlobalObject.js
@@ -0,0 +1,9 @@
+//Global Objects
+    console.log();  // GLOBAL SCOPE :Can be accessed anywhere to log anything on console.
+    setTimeout(); // Used to call a function after a delay.
+    clearTimeout();
+    setInterval(); //Repeatedly call a function after a given delay
+    clearInterval(); //Stop a function from being called repeatedly
+    window // In browser all global objects can be accessed using window object but in Node window doesn't work
+    global // In Node we use global instead of window to access global objects
+    
