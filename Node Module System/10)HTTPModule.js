a6e83de57b5dc54a2f23c5eab3acc5ff45f4f826
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2022-11-12 19:55:49 -0500

    2.j) Node Module System: HTTP Module

---
+++ Node Module System/10)HTTPModule.js
@@ -0,0 +1,19 @@
+//HTTP Module used for creating Networking Applications.
+//Internally the Express framework is built on top of HTTP Module in Node.
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World");
+    res.end();
+  }
+
+  if (req.url == "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});
+
+server.listen(3000);
+
+console.log("Listening on Port 3000...");
